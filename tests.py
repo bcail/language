50fa8a7da6b6c6f2ac3fbdc211d4258f241a8755
Ben <bcail@speedymail.org>
2023-01-16 14:47:01 -0500

    try to fix newline character on windows

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import os
+from os import linesep as LSEP
 import platform
 import subprocess
 import tempfile
@@ -592,9 +593,9 @@ class CompileTests(unittest.TestCase):
             {'src': '(do (print 1) (print 2))', 'output': '12'},
             {'src': '(do (print 1) (if (< 1 2) (print 1) (print 2)))', 'output': '11'},
             {'src': '(print (do (print 1) (if (< 1 2) (print 1) (print 2)) "3"))', 'output': '113'},
-            {'src': '(do (println "line1") (println "line2"))', 'output': 'line1\nline2\n'},
-            {'src': '(print (do (println "output") 2))', 'output': 'output\n2'},
-            {'src': '(print (do (println "output") "return"))', 'output': 'output\nreturn'},
+            {'src': '(do (println "line1") (println "line2"))', 'output': f'line1{LSEP}line2{LSEP}'},
+            {'src': '(print (do (println "output") 2))', 'output': f'output{LSEP}2'},
+            {'src': '(print (do (println "output") "return"))', 'output': f'output{LSEP}return'},
         ]
         for test in tests:
             with self.subTest(test=test):
@@ -623,7 +624,7 @@ class CompileTests(unittest.TestCase):
             {'src': '(loop [n 0] (if (> n 2) (print n) (let [y 1] (recur (+ n y)))))', 'output': '3'},
             {'src': '(let [b 2] (loop [n 0] (if (> n b) (print n) (let [y 1] (recur (+ n y))))))', 'output': '3'},
             {'src': '(loop [n 0] (if (> n 2) (print "done") (do (print n) (recur (+ n 1)))))', 'output': '012done'},
-            {'src': '(loop [n 0] (do (print n) (print "    ") (println (/ (* 5 (- n 32)) 9)) (if (< n 70) (recur (+ 20 n)))))', 'output': '0    -17.7778\n20    -6.66667\n40    4.44444\n60    15.5556\n80    26.6667\n'},
+            {'src': '(loop [n 0] (do (print n) (print "    ") (println (/ (* 5 (- n 32)) 9)) (if (< n 70) (recur (+ 20 n)))))', 'output': f'0    -17.7778{LSEP}20    -6.66667{LSEP}40    4.44444{LSEP}60    15.5556{LSEP}80    26.6667{LSEP}'},
         ]
         for test in tests:
             with self.subTest(test=test):
@@ -676,7 +677,7 @@ class CompileTests(unittest.TestCase):
             {'src': '(print (loop [line (read-line)] (if (= nil line) "done" (recur (read-line)))))', 'input': 'line', 'output': 'done'},
             {'src': '(print (loop [line (read-line)] (if (= nil line) ["done"] (recur (read-line)))))', 'input': 'line', 'output': '[done]'},
             {'src': '(print (loop [line (read-line)] (if (= nil line) {"a" "b"} (recur (read-line)))))', 'input': 'line', 'output': '{a b}'},
-            {'src': '(print (loop [line (read-line)] (if (= nil line) "done" (do (print line) (recur (read-line))))))', 'input': 'line1\nline2\n', 'output': 'line1line2done'},
+            {'src': '(print (loop [line (read-line)] (if (= nil line) "done" (do (print line) (recur (read-line))))))', 'input': f'line1{LSEP}line2{LSEP}', 'output': 'line1line2done'},
         ]
         for test in tests:
             with self.subTest(test=test):
@@ -684,9 +685,9 @@ class CompileTests(unittest.TestCase):
 
     def test_other(self):
         tests = [
-            {'src': '(println "hello")', 'output': 'hello\n'},
-            {'src': '(print (read-line))', 'input': 'line\n', 'output': 'line'},
-            {'src': '(print (read-line))', 'input': '\n', 'output': ''},
+            {'src': '(println "hello")', 'output': f'hello{LSEP}'},
+            {'src': '(print (read-line))', 'input': f'line{LSEP}', 'output': 'line'},
+            {'src': '(print (read-line))', 'input': LSEP, 'output': ''},
             {'src': '(print (read-line))', 'input': '', 'output': 'nil'},
         ]
         for test in tests:
