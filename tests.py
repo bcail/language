6de4a4d87cc39d95f797dcda740939187c80aa6c
Ben Cail <bcail@crossway.org>
2023-05-19 15:57:27 -0400

    try running sqlite tests without -static

--- tests.py
+++ tests.py
@@ -395,7 +395,7 @@ def _run_test(test, assert_equal, sqlite=False):
             program_filename = os.path.join(tmp, env_name)
 
             if sqlite:
-                compile_cmd = cc_cmd + ['-o', program_filename, os.path.join('lib', 'sqlite3.c'), c_filename, '-static', '-I ./include', '-Wl,-lm,-lpthread,-ldl']
+                compile_cmd = cc_cmd + ['-o', program_filename, os.path.join('lib', 'sqlite3.c'), c_filename, '-I ./include', '-Wl,-lm,-lpthread,-ldl']
             else:
                 compile_cmd = cc_cmd + ['-o', program_filename, c_filename, '-Wl,-lm']
             try:
